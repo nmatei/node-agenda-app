051061d90ee8ba581f102c908b4d2dd9746c23e4
Matei Nicolae <matei.nick@gmail.com>
2018-06-25 19:40:02 +0300

    group all functions in json

--- public/js/phone-book.js
+++ public/js/phone-book.js
@@ -8,110 +8,112 @@ const API_URL = {
     DELETE: '/agenda/delete'
 };
 
-function getRow(person) {
-    return "<tr>" +
-        "<td>" + person.firstName + "</td>" +
-        "<td>" + person.lastName + "</td>" +
-        "<td>" + person.phone + "</td>" +
-        `<td>` +
+window.PhoneBook = {
+    getRow: function(person) {
+        return "<tr>" +
+            "<td>" + person.firstName + "</td>" +
+            "<td>" + person.lastName + "</td>" +
+            "<td>" + person.phone + "</td>" +
+            `<td>` +
             `<a href='#' data-id='${person.id}' class='delete'>&#10006;</a> ` +
             `<a href='#' data-id='${person.id}' class='edit'>&#9998;</a>` +
-        `</td>` +
-        "</tr>";
-}
+            `</td>` +
+            "</tr>";
+    },
 
-function getActionRow() {
-    return '<tr>' +
-        '<td><input type="text" required name="firstName" placeholder="Enter first name"></td>' +
-        '<td><input type="text" name="lastName" placeholder="Enter last name"></td>' +
-        '<td><input type="text" required name="phone" placeholder="Enter phone"></td>' +
-        '<td><button type="submit">Save</button></td>' +
-        '</tr>';
-}
+    load: function () {
+        $.ajax({
+            url: API_URL.READ,
+            method: "GET"
+        }).done(function (persons) {
+            console.info('done:', persons);
+            display(persons);
+        });
+    },
 
-function loadContacts() {
-    $.ajax({
-        url: API_URL.READ,
-        method: "GET"
-    }).done(function (persons) {
-        console.info('done:', persons);
-        display(persons);
-    });
-}
+    getActionRow: function() {
+        return '<tr>' +
+            '<td><input type="text" required name="firstName" placeholder="Enter first name"></td>' +
+            '<td><input type="text" name="lastName" placeholder="Enter last name"></td>' +
+            '<td><input type="text" required name="phone" placeholder="Enter phone"></td>' +
+            '<td><button type="submit">Save</button></td>' +
+            '</tr>';
+    },
 
-function deleteContact(id) {
-    $.ajax({
-        url: API_URL.DELETE,
-        method: "POST",
-        data: {
-            id: id
-        }
-    }).done(function (response) {
-        if (response.success) {
-            loadContacts();
-        }
-    });
-}
+    delete: function(id) {
+        $.ajax({
+            url: API_URL.DELETE,
+            method: "POST",
+            data: {
+                id: id
+            }
+        }).done(function (response) {
+            if (response.success) {
+                PhoneBook.load();
+            }
+        });
+    },
 
-function addContact(person) {
-    $.ajax({
-        url: API_URL.CREATE,
-        method: "POST",
-        data: person
-    }).done(function (response) {
-        if (response.success) {
-            loadContacts();
-        }
-    });
-}
+    add: function(person) {
+        $.ajax({
+            url: API_URL.CREATE,
+            method: "POST",
+            data: person
+        }).done(function (response) {
+            if (response.success) {
+                PhoneBook.load();
+            }
+        });
+    },
 
-function saveContact(person) {
-    $.ajax({
-        url: API_URL.UPDATE,
-        method: "POST",
-        data: person
-    }).done(function (response) {
-        if (response.success) {
-            editId = '';
-            loadContacts();
-        }
-    });
-}
+    save: function(person) {
+        $.ajax({
+            url: API_URL.UPDATE,
+            method: "POST",
+            data: person
+        }).done(function (response) {
+            if (response.success) {
+                editId = '';
+                PhoneBook.load();
+            }
+        });
+    },
 
-function bindEvents() {
-    $('#phone-book tbody').delegate('a.edit', 'click', function () {
-        var id = $(this).data('id');
-        editContact(id);
-    });
+    bindEvents: function() {
+        $('#phone-book tbody').delegate('a.edit', 'click', function () {
+            var id = $(this).data('id');
+            PhoneBook.edit(id);
+        });
 
-    $('#phone-book tbody').delegate('a.delete', 'click', function () {
-        var id = $(this).data('id');
-        console.info('click on ', this, id);
-        deleteContact(id);
-    });
+        $('#phone-book tbody').delegate('a.delete', 'click', function () {
+            var id = $(this).data('id');
+            console.info('click on ', this, id);
+            PhoneBook.delete(id);
+        });
 
-    $( ".add-form" ).submit(function() {
-        const person = {
-            firstName: $('input[name=firstName]').val(),
-            lastName: $('input[name=lastName]').val(),
-            phone: $('input[name=phone]').val()
-        };
+        $( ".add-form" ).submit(function() {
+            const person = {
+                firstName: $('input[name=firstName]').val(),
+                lastName: $('input[name=lastName]').val(),
+                phone: $('input[name=phone]').val()
+            };
 
-        if (editId) {
-            person.id = editId;
-            saveContact(person);
-        } else {
-            addContact(person);
-        }
-    });
-}
+            if (editId) {
+                person.id = editId;
+                PhoneBook.save(person);
+            } else {
+                PhoneBook.add(person);
+            }
+        });
+    }
+};
 
 function display(persons) {
     window.persons = persons;
     var rows = '';
 
-    persons.forEach(person => rows += getRow(person));
-    rows += getActionRow();
+    persons.forEach(person => rows += PhoneBook.getRow(person));
+    rows += PhoneBook.getActionRow();
     $('#phone-book tbody').html(rows);
 }
 
@@ -140,5 +142,5 @@ function cancelEdit(button) {
 
 var persons = [];
 console.info('loading persons');
-loadContacts();
-bindEvents();
+PhoneBook.load();
+PhoneBook.bindEvents();
