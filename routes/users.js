bc8eda9c8d382d5a5c13a7a5b18b4224f8c1c278
Nicolae Matei <nmatei@sdl.com>
2019-04-15 21:55:36 +0300

    save persons

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
@@ -12,6 +13,17 @@ router.post('/add', function(req, res, next) {
   var lastName = req.body.lastName;
   var phone = req.body.phone;
   console.warn('add', firstName, lastName, phone);
+  var persons = require('../public/data/persons.json');
+  
+  persons.push({
+    firstName,
+    lastName,
+    phone
+  });
+  
+  var str = JSON.stringify(persons, null, 2);
+  fs.writeFileSync('./public/data/persons.json', str);
+
   // TODO save this data in persons.json
   res.json({
     success: true,
